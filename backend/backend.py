c3b90e7df7b6c873acc59dfe4c8b74e2ca86aa66
Rahul Kodali <rahulkodali@Rahuls-MacBook-Air.local>
2025-01-25 14:36:08 -0600

    doin stuff

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+@app.route('/api/route', methods=['POST'])
+def route():
+   data = request.json
+   
+
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
