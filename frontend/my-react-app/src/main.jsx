28bab1dc6998ef04a00fdcd8373dbc1735ac0106
Krish Shah <krishshah765@yahoo.com>
2025-01-25 15:18:07 -0600

    front end working

--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Theme } from "@radix-ui/themes";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Theme 
+      appearance="dark" 
+      accentColor="tomato"
+    >
+      <App />
+    </Theme>
   </StrictMode>,
 )
