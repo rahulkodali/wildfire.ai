2bc7070d70c2b11138cac4f20b8d1736f3ea1a6a
ArmaanMediratta <armaan@taamba.com>
2025-01-25 14:38:42 -0600

    format

--- backend/backend.py
+++ backend/backend.py
@@ -1,15 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/api/route', methods=['POST'])
+@app.route("/api/route", methods=["POST"])
 def route():
-   data = request.json
-   
+    data = request.json
 
-@app.route('/hello/<name>')
+
+@app.route("/hello/<name>")
 def hello_name(name):
-   return 'Hello %s!' % name
+    return "Hello %s!" % name
 
-if __name__ == '__main__':
-   app.run()
+
+if __name__ == "__main__":
+    app.run()
