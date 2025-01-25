98de02758c47c0c6bfecbd31daba5e689639f544
ArmaanMediratta <armaan@taamba.com>
2025-01-25 17:36:10 -0600

    finals things

--- backend/test.py
+++ backend/test.py
@@ -6,45 +6,6 @@ import math
 from geopy.distance import geodesic
 import random
 
-# def route(pointA, pointB, polygon):
-#     load_dotenv()
-
-#     # schema POINT A, POINT B, COORDS OF POLYGON
-
-
-#     # iterate/map thru and give a list of coods
-
-#     body = {
-#         "coordinates": [[pointA[0], pointA[1]], [pointB[0], pointB[1]]],
-#         "options": {
-#             "avoid_polygons": {
-#                 "type": "Polygon",
-#                 "coordinates": [polygon],
-#             }
-#         },
-#     }
-
-#     headers = {
-#         "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
-#         "Authorization": os.getenv("API_KEY_ROUTE"),
-#         "Content-Type": "application/json; charset=utf-8",
-#     }
-
-#     call = requests.post(
-#         "https://api.openrouteservice.org/v2/directions/driving-car/json",
-#         json=body,
-#         headers=headers,
-#     )
-
-#     print(call.status_code, call.reason)
-#     print(call.text)
-
-# route([-96.339920, 30.597614],[-96.340612, 30.612040],[[-96.3461393,30.6083298],
-#       [-96.3450081, 30.6073311],
-#       [-96.3415942, 30.6118933],
-#       [-96.333934, 30.5971139],
-#       [-96.3461393,30.6083298] ])
-
 
 def test():
     url = "https://incidents.fire.ca.gov/umbraco/api/IncidentApi/GeoJsonList?inactive=true"
@@ -79,20 +40,58 @@ def jag(dict_vals):
         acres_burned = 10425
         radius_miles = math.sqrt(acres_burned * 43560) / 5280
 
-        for angle in range(0, 360, 45):
+        for angle in range(45, 360, 45):
             distance = geodesic(miles=radius_miles).destination(center, angle)
             lat, lon = distance.latitude, distance.longitude
 
             jagged_lat = lat + random.uniform(-0.005, 0.005)
             jagged_lon = lon + random.uniform(-0.005, 0.005)
 
-            points.append([jagged_lat, jagged_lon])
+            points.append([jagged_lon, jagged_lat])
+
+        points.append(points[0])
 
         dict_vals[fire_name] = points
 
     return list(dict_vals.values())
 
 
-val = test()
-trueVal = jag(val)
-print(trueVal[0])
+def route(pointA, pointB, polygon):
+    load_dotenv()
+
+    # schema POINT A, POINT B, COORDS OF POLYGON
+
+    # iterate/map thru and give a list of coods
+
+    body = {
+        "coordinates": [[pointA[0], pointA[1]], [pointB[0], pointB[1]]],
+        "options": {
+            "avoid_polygons": {
+                "type": "MultiPolygon",
+                "coordinates": [polygon],
+            }
+        },
+    }
+
+    headers = {
+        "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+        "Authorization": os.getenv("API_KEY_ROUTE"),
+        "Content-Type": "application/json; charset=utf-8",
+    }
+
+    call = requests.post(
+        "https://api.openrouteservice.org/v2/directions/driving-car/json",
+        json=body,
+        headers=headers,
+    )
+
+    print(call.status_code, call.reason)
+    print(call.text)
+
+
+route([-96.339920, 30.597614], [-96.340612, 30.612040], jag(test()))
+
+
+# val = test()
+# trueVal = jag(val)
+# print(trueVal)
